fda4903ceeb1ff6c534bba6698f26f737bc61373
LarsGJobloop <lars.gunnar@jobloop.no>
2024-05-29 11:11:58 +0200

    add function to execute when input fields changes

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Hello!');
+function logToConsole() {
+    console.log("Input changed")
+}
+
+// Hent ut alle elemnter med klasse navn "input-element"
+const inputElements = document.getElementsByClassName("input-element")
+
+// For løkker ->  For hvert element i samlingen inputElements gjør logikken i kodeblokken
+for (const input of inputElements) {
+    input.addEventListener("input", logToConsole)
+}
